e8129d101b05e20f3b032ea81de6ac6ab0b49f5b
JohisArias <johismn9-3@hotmail.com>
2016-06-13 05:04:45 -0500

    Delete ForkHoja1.c

--- ForkHoja1.c
+++
@@ -1,8 +0,0 @@
-//Ejercicio 1 (hojas)
-#include<stdio.h>
-#include<unistd.h>
-int main()
-{
-	printf("El identificador de este proceso es PID = %d\n", (int) getpid());
-	printf("El identificador del proceso padre es PPID = %d\n", (int) getppid());
-}//fin main
